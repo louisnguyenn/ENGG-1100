b8ad41acb51e82be57b3391d060f5071473d5a4e
Louis Nguyen <louis.nguyen550@gmail.com>
2025-10-23 20:12:46 -0400

    prototype code

--- TBWC_F25.ino
+++ TBWC_F25.ino
@@ -15,42 +15,70 @@ void setup() {
   pinMode(enA, OUTPUT);
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
-  pinMode(enB, OUTPUT);
-  pinMode(in3, OUTPUT);
-  pinMode(in4, OUTPUT);
+  // pinMode(enB, OUTPUT);
+  // pinMode(in3, OUTPUT);
+  // pinMode(in4, OUTPUT);
   Serial.begin(9600); // transferring 9600 bits per second (the maximum)
 
   // PWM range: 0-255 (speed, 255 is max)
 
-  // FORWARD
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, HIGH);
-  analogWrite(enA, 255);
-  // analogWrite(enB, 255);
-  delay(2000);
+  // // FORWARD
+  // digitalWrite(in1, HIGH);
+  // digitalWrite(in2, LOW);
+  // // digitalWrite(in3, LOW);
+  // // digitalWrite(in4, HIGH);
+  // analogWrite(enA, 200);
+  // // analogWrite(enB, 255);
+  // delay(2000);
 
-  // BACKWARDS
-  digitalWrite(in1, LOW);
-  digitalWrite(in2, HIGH);
-  digitalWrite(in3, HIGH);
-  digitalWrite(in4, LOW);
-  analogWrite(enA, 255);
-  // analogWrite(enB, 255);
-  delay(2000);
+  // // BACKWARDS
+  // digitalWrite(in1, LOW);
+  // digitalWrite(in2, HIGH);
+  // // digitalWrite(in3, HIGH);
+  // // digitalWrite(in4, LOW);
+  // analogWrite(enA, 200);
+  // // analogWrite(enB, 255);
+  // delay(2000);
 
-  // STOP
-  digitalWrite(in1, LOW); // set all pins to LOW
-  digitalWrite(in2, LOW);
-  digitalWrite(in3, LOW);
-  digitalWrite(in4, LOW);
-  analogWrite(enA, 0);  // set speed to low
-  // analogWrite(enB, 0);
-  delay(2000);  // stop for 2 seconds
+  // // STOP
+  // digitalWrite(in1, LOW); // set all pins to LOW
+  // digitalWrite(in2, LOW);
+  // // digitalWrite(in3, LOW);
+  // // digitalWrite(in4, LOW);
+  // analogWrite(enA, 0);  // set speed to low
+  // // analogWrite(enB, 0);
+  // delay(2000);  // stop for 2 seconds
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // STOP
+  digitalWrite(in1, LOW); // set all pins to LOW
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 0);  // set speed to low
+  delay(2000);
 
+  // FORWARD
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enA, 255);
+  delay(6000);
+
+  // STOP
+  digitalWrite(in1, LOW); // set all pins to LOW
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 0);  // set speed to low
+  delay(6000);
+
+  // BACKWARDS
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 2);
+  delay(6000);
+
+  // STOP
+  digitalWrite(in1, LOW); // set all pins to LOW
+  digitalWrite(in2, LOW);
+  analogWrite(enA, 0);  // set speed to low
+  delay(6000);
 }
