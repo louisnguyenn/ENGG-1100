c9ce5fdb0d270774458e2e2715d9f072ef7550b5
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-19 17:14:25 -0500

    comment on final code

--- code/final/TBWC_F25_Final1.ino
+++ code/final/TBWC_F25_Final1.ino
@@ -1,12 +1,13 @@
 #include <Servo.h> 
 
+// Author: Louis Nguyen (1321410)
+
 const int enA = 5;
 const int in1 = 6;
 const int in2 = 7;
 
 Servo servoMotor;
 
-// max 255
 int speed = 245;
 
 void setup() {
@@ -19,22 +20,43 @@ void setup() {
 }
 
 void loop() {
+  // Set servomotor position to 0
+  // Effectiveness: 10/10
   servoMotor.write(0);
   
+  //
+  // Successfully moved forward for the allocated number of time
+  // Effectiveness: 10/10 
+  //
+  // Move forward
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   analogWrite(enA, speed);
   delay(3900);
 
-  // STOP
+  //
+  // Successfully stopped for the allocated number of time
+  // Effectiveness: 10/10
+  //
+  // Stop
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   analogWrite(enA, 0);
   delay(1000);
 
-  // REVERSE
+  //
+  // Successfully reversed for the allocated number of time
+  // Effectiveness: 10/10
+  //
+  // Reverse
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
   analogWrite(enA, speed);
   delay(10000);
+
+  // Overall effectiveness: 10/10
+  // The code was very effective, as there weren't any issues with the Arduino control during test and performance day.
+  
+  // Possible Improvements for the future:
+  // For the future, more testing could be done with the code and chassis before our performance day. For example, we should've tested various speeds to ensure our TBWC was able to traverse the course at a fast time while also ensuring it doesn't cause any drifting issues.
 }
--- code/final/TBWC_F25_Final2.ino
+++ code/final/TBWC_F25_Final2.ino
@@ -1,5 +1,7 @@
 #include <Servo.h> 
 
+// Author: Louis Nguyen (1321410)
+
 const int enA = 5;
 const int in1 = 6;
 const int in2 = 7;
@@ -18,28 +20,45 @@ void setup() {
 }
 
 void loop() {
-  // DRIVE FORWARD
+  //
+  // Successfully move forward for the allocated number of time
+  // Effectiveness: 10/10
+  //
   digitalWrite(in1, LOW);
   digitalWrite(in2, HIGH);
   analogWrite(enA, speed);
   delay(3950);
 
-  // STOP
+  //
+  // Successfully stopped for the allocated number of time
+  // Effectiveness: 10/10
+  //
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   analogWrite(enA, 0);
   delay(2000);
 
-  // ROTATE SERVO
+  //
+  // Successfully rotated the servo motor, activating the launching mechanism
+  // Effectiveness: 10/10
+  //
   servoMotor.write(180);  // rotate servo 180 degrees
   delay(1000);
   servoMotor.write(0);  // rotate servo 0 degrees
   delay(1000);
   
-  // FINAL STOP
+  //
+  // Successfully stopped for the allocated number of time
+  // Effectiveness: 10/10
+  //
   digitalWrite(in1, LOW);
   digitalWrite(in2, LOW);
   analogWrite(enA, 0);
+  delay(10000);
 
-  while (true); // stop forever
+  // Overall effectiveness: 10/10
+  // The code was very effective, as everything went smoothly during testing and performance day. 
+  
+  // Possible Improvements for the future:
+  // For the future, more testing could have been done at the projectile launch course, where we should've tested the optimal speed for the TBWC to traverse over the bump and launch the projectile. During our performance demonstration, the speed was set too high, causing the projectile to fall out of it's place, resulting in a lower mark. Overall, the code was effective and successful, but more testing should have been done to ensure that the speed and timing went more smoothly.
 }
